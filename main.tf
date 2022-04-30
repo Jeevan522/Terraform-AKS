0383be188b301e6ce00aed0ebb66efc143bb6a7a
Jeevan522 <jeevan0522@gmail.com>
2022-04-30 17:10:21 +0530

    Plan

--- main.tf
+++ main.tf
@@ -13,14 +13,7 @@
 #   features {}
 # }
 
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "=3.0.0"
-#     }
-#   }
-# }
+
  terraform {
    required_version = "=1.1.9"
   backend "azurerm" {
