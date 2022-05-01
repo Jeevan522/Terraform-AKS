6d400e1222adab73daa0da4de2d7d53d4eeacdd9
Jeevan522 <jeevan0522@gmail.com>
2022-05-01 18:37:49 +0530

    AKS

--- main.tf
+++ main.tf
@@ -48,3 +48,89 @@ data "azurerm_subnet" "kubesubnet" {
   resource_group_name  = azurerm_resource_group.rg-k8s.name
   depends_on = [azurerm_virtual_network.k8s-network]
 }
+
+# Log Analytics
+resource "azurerm_log_analytics_workspace" "k8s-law" {
+  name                = "log-k8s"
+  location            = azurerm_resource_group.rg-k8s.location
+  resource_group_name = azurerm_resource_group.rg-k8s.name
+  sku                 = "PerGB2018"
+}
+
+resource "azurerm_log_analytics_solution" "ems-las" {
+  solution_name         = "ContainerInsights"
+  location              = azurerm_resource_group.rg-k8s.location
+  resource_group_name   = azurerm_resource_group.rg-k8s.name
+  workspace_resource_id = azurerm_log_analytics_workspace.k8s-law.id
+  workspace_name        = azurerm_log_analytics_workspace.k8s-law.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
+}
+#AKS
+
+resource "azurerm_kubernetes_cluster" "k8s-aks" {
+  #for_each = var.ems_app_gateway
+
+  name                = var.aks-name
+  location            = var.rg_location
+  resource_group_name = azurerm_resource_group.rg-k8s.name
+  dns_prefix          = var.dns_prefix
+  kubernetes_version  = var.kubernetes_version
+  node_resource_group = var.node_resource_group
+
+
+  # linux_profile {
+  #   admin_username = "ubuntu"
+
+  #   ssh_key {
+  #     key_data = var.ssh_public_key
+  #   }
+  # }
+
+  
+  network_profile {
+    network_plugin    = "azure"
+  }
+  
+  # network_profile {
+  #   network_plugin    = "kubenet"
+  #   load_balancer_sku = "Standard"
+  # }
+  
+  
+  
+
+  default_node_pool {
+    name       = "agentpool"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+    #type       = "VirtualMachineScaleSets"
+	  vnet_subnet_id  = data.azurerm_subnet.kubesubnet.id
+	
+  }
+  service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
+  addon_profile {
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.k8s-law.id
+    }
+    http_application_routing {
+      enabled = false
+    }
+    kube_dashboard {
+      enabled = true
+    }
+  }
+  depends_on = [azurerm_virtual_network.k8s-network]
+  #depends_on = [azurerm_virtual_network.ems-network, azurerm_application_gateway.ems-gateway]
+
+  tags = {
+    Environment = local.environment
+  }
+}
--- variables.tf
+++ variables.tf
@@ -17,3 +17,27 @@ variable virtual_network_address_prefix {
 variable aks_subnet_address_prefix {
   default = "10.240.1.0/24"
 }
+
+variable aks-name {
+    default = "aks-sample"
+}
+
+variable kubernetes_version{
+    default = "1.22.4"
+}
+
+variable dns_prefix {
+  default = "aks-sample-1"
+}
+
+variable node_resource_group {
+  default = "k8s"
+}
+
+variable client_id {
+  default = "b23e510f-229b-4321-bce7-4dc46ce22c1d"
+}
+
+variable client_secret {
+  default = "b23e510f-229b-4321-bce7-4dc46ce22c1d"
+}
