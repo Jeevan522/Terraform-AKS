44a7f6e187a97be77437e09bba887b2e19698251
Jeevan522 <jeevan0522@gmail.com>
2022-05-02 22:07:32 +0530

    updated agw

--- main.tf
+++ main.tf
@@ -259,3 +259,20 @@ resource "azurerm_user_assigned_identity" "test_identity" {
   location            = azurerm_resource_group.rg-k8s.location
   resource_group_name = azurerm_resource_group.rg-k8s.name
 }
+
+# Contributor to AGW
+resource "azurerm_role_assignment" "contributor" {
+  
+  scope                = azurerm_application_gateway.agw.id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_user_assigned_identity.test_Identity.principal_id
+  depends_on           = [azurerm_user_assigned_identity.test_Identity, azurerm_application_gateway.agw]
+}
+
+# Reader to AGW
+resource "azurerm_role_assignment" "reader" {
+  scope                = azurerm_application_gateway.agw.id
+  role_definition_name = "Reader"
+  principal_id         = azurerm_user_assigned_identity.test_Identity.principal_id
+  depends_on           = [azurerm_user_assigned_identity.test_Identity, azurerm_application_gateway.agw]
+}
